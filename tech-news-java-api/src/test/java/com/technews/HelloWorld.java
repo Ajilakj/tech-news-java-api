a476b91e24d48120a87831e3653217e816f8e4ce
AKJ <kjajila@gmail.com>
2022-12-19 11:59:33 -0500

    complete variable types

--- tech-news-java-api/src/test/java/com/technews/HelloWorld.java
+++ tech-news-java-api/src/test/java/com/technews/HelloWorld.java
@@ -3,5 +3,6 @@ package com.technews;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+
     }
 }
---
+++ tech-news-java-api/src/test/java/com/technews/VariableTypes.java
@@ -0,0 +1,18 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        boolean isFavorite = false;
+        boolean notIsFavorite = !isFavorite;
+        String favString = "not really";
+        String name = "Ajila";
+        int age = 2016 + 69;
+        double price = 1999.99;
+        System.out.println(isFavorite);
+        System.out.println(notIsFavorite);
+        System.out.println(favString);
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(price);
+    }
+}
